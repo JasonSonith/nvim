d846548d2456d3fac534c0fcd04d962a825b6821
JasonSonith <JasonSonith1@gmail.com>
2026-05-06 16:59:45 -0500

    reconfigured tmux

--- nvim/lua/highlights.lua
+++ nvim/lua/highlights.lua
@@ -1,6 +1,8 @@
 local function apply_highlights()
-  vim.api.nvim_set_hl(0, "LineNr", { fg = "#5c6370" })
-  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#e5c07b", bold = true })
+  vim.api.nvim_set_hl(0, "LineNr", { fg = "#a6adc8" })
+  vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#a6adc8" })
+  vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#a6adc8" })
+  vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#f9e2af", bold = true })
 end
 
 apply_highlights()
